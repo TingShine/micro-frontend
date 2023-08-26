ee3c65f1e8ee4d9480bce53b7c025f9a8ae0eb36
TingShine <791405318@qq.com>
2023-08-26 11:40:29 +0800

    ci: 新增依赖构建

--- packages/main-vue3/vite.config.js
+++ packages/main-vue3/vite.config.js
@@ -1,28 +1,54 @@
 import { defineConfig } from "vite";
 import { visualizer } from "rollup-plugin-visualizer";
 import vue from "@vitejs/plugin-vue";
-import Components from 'unplugin-vue-components/vite';
-import AutoImport from 'unplugin-auto-import/vite';
-import { TDesignResolver } from 'unplugin-vue-components/resolvers';
+import Components from "unplugin-vue-components/vite";
+import AutoImport from "unplugin-auto-import/vite";
+import { TDesignResolver } from "unplugin-vue-components/resolvers";
+import legacy from "@vitejs/plugin-legacy";
+import { viteExternalsPlugin } from "vite-plugin-externals";
+import { createHtmlPlugin } from 'vite-plugin-html';
 
+import { externalsObj, externalsScript } from '../../scripts';
+
+console.log(externalsScript);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), AutoImport({
-    resolvers: [TDesignResolver({library: 'vue-next'})]
-  }),
-Components({
-  resolvers: [TDesignResolver({library: 'vue-next'})]
-})],
+  base: "./",
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [TDesignResolver({ library: "vue-next" })],
+    }),
+    Components({
+      resolvers: [TDesignResolver({ library: "vue-next" })],
+    }),
+    legacy({
+      targets: ["defaults", "not IE 11"],
+    }),
+    viteExternalsPlugin(
+      { ...externalsObj },
+      { disableInServe: true }
+    ),
+    createHtmlPlugin({
+      template: 'index.html',
+      inject: {
+        data: {
+          title: 'Shine',
+          injectScripts: externalsScript
+        }
+      }
+    })
+  ],
   build: {
     rollupOptions: {
       plugins: [
-        visualizer({
-          open: true,
-          template: "treemap",
-          gzipSize: true,
-          brotliSize: true,
-        }),
+        // visualizer({
+        //   open: true,
+        //   template: "treemap",
+        //   gzipSize: true,
+        //   brotliSize: true,
+        // }),
       ],
     },
   },
