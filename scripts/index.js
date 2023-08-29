afeaab7b1328b4f5a90c8b885869694baa640934
TingShine <791405318@qq.com>
2023-08-29 22:19:49 +0800

    perf: 优化公共依赖加载方式

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,8 @@ import mainPackage from "../package.json";
 import crypto from "crypto";
 import axios from "axios";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 // cdn前缀
 const protocol = "https";
 const cdnPrefix = "cdn.superting.cn";
@@ -44,22 +46,41 @@ hash.update(externalsStr);
 const md5 = hash.digest("hex").slice(0, 8);
 
 const cdnPath = `${protocol}://${cdnPrefix}/${mainAppName}/${subAppName}/resource-${md5}.js`;
-export const externalsScript = externalsStr && process.env.NODE_ENV === "production"
-  ? `<script src='${cdnPath}'></script>`
-  : "";
+export const externalsScript =
+  externalsStr && process.env.NODE_ENV === "production"
+    ? `<script src='${cdnPath}'></script>`
+    : "";
+
+// 生成env.config.js配置
+let envMD5 = "";
+const envPath = `${subApplicationDir}/public/env.config.js`;
+if (isProduction && existsSync(envPath)) {
+    const envHash = crypto.createHash("md5");
+    envHash.update(
+      readFileSync(subPackageJsonPath, { encoding: "utf8", flag: "r" })
+    );
+    const md5 = envHash.digest("hex").slice(0, 8);
+    envMD5 = md5;
+}
+export { envMD5 };
 
 // combine方案
-if (externalsStr && process.env.NODE_ENV === "production") {
-  const downloadUrl = `https://cdn.jsdelivr.net/combine/${externalsStr}`;
-  console.log(`Downloading Resource ==> ${downloadUrl}`);
-  const resources = await axios
-    .get(downloadUrl)
-    .then((response) => response.data);
+if (externalsStr && isProduction) {
+  try {
+    const res = await axios.head(cdnPath);
+    if (res.status !== 200) throw new Error("资源不存在");
+  } catch (e) {
+    const downloadUrl = `https://cdn.jsdelivr.net/combine/${externalsStr}`;
+    console.log(`Downloading Resource ==> ${downloadUrl}`);
+    const resources = await axios
+      .get(downloadUrl)
+      .then((response) => response.data);
 
-  const resourceDir = `${subApplicationDir}/cdn`;
-  if (!existsSync(resourceDir)) {
-    mkdirSync(resourceDir, { recursive: true });
+    const resourceDir = `${subApplicationDir}/cdn`;
+    if (!existsSync(resourceDir)) {
+      mkdirSync(resourceDir, { recursive: true });
+    }
+    writeFileSync(`${resourceDir}/resource-${md5}.js`, resources);
+    console.log("Resources download finished, Start to build...");
   }
-  writeFileSync(`${resourceDir}/resource-${md5}.js`, resources);
-  console.log("Resources download finished, Start to build...");
 }
